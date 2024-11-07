9928a0534c75cf828b169d533d4cd39d2b485caa
voltarocket <voltarocket@gmail.com>
2024-11-07 14:03:24 +0300

    commit

--- components/game/game-field.jsx
+++ components/game/game-field.jsx
@@ -2,43 +2,102 @@ import clsx from "clsx";
 import { ZeroIcon } from "./icons/zero-icon";
 import { CrossIcon } from "./icons/cross-icon";
 import { UiButton } from "../uikit/ui-button";
+import { useState } from "react";
+
+const GAME_SYMBOLS = {
+  ZERO: "zero",
+  CROSS: "cross",
+  TRIANGLE: "triangle",
+  SQUARE: "square",
+};
+
+const MOVE_ORDER = [
+  GAME_SYMBOLS.CROSS,
+  GAME_SYMBOLS.ZERO,
+  GAME_SYMBOLS.TRIANGLE,
+  GAME_SYMBOLS.SQUARE
+
+];
+
+function getNextMove(currentMove) {
+  const nextMoveIndex = MOVE_ORDER.indexOf(currentMove) + 1;
+  return MOVE_ORDER[nextMoveIndex] ?? MOVE_ORDER[0];
+}
 
 export function GameField({ className }) {
 
-  const cells = new Array(19 * 19).fill(null);
 
+  const [cells, setCells] = useState(() => new Array(19 * 19).fill(null));
+  const [currentMove, setCurrentMove] = useState(GAME_SYMBOLS.CROSS);
+
+  const nextMove = getNextMove(currentMove)
+
+  const actions = (
+    <>
+      <UiButton size="md" variant="primary">
+        Ничья
+      </UiButton>
+      <UiButton size="md" variant="outline">
+        Сдаться
+      </UiButton>
+    </>
+  );
+
+  return (
+    <GameFieldLayout className={className}>
+      <GameMoveInfo actions={actions} />
+      <GameGrid>
+        {cells.map((_, index) => (
+          <GameCell key={index}></GameCell>
+        ))}
+      </GameGrid>
+    </GameFieldLayout>
+  );
+}
+
+function GameCell({ children }) {
+  return (
+    <button className="border border-slate-200 -ml-px -mt-px flex items-center justify-center">
+      {children}
+    </button>
+  );
+}
+
+function GameFieldLayout({ children, className }) {
   return (
     <div
       className={clsx(
         className,
         "bg-white rounded-2xl shadow-md px-8 pt-5 pb-7"
       )}
-    >
-      <div className="flex gap-3 items-center">
-        <div className="mr-auto">
-          <div className="flex items-center gap-1 text-xl leading-tight text-slate-900">
-            Ход: <ZeroIcon className="w-5 h-5" />
-          </div>
-          <div className="flex items-center gap-1 text-xs leading-tight text-slate-400">
-            Следующий: <CrossIcon />
-          </div>
+    >{children}</div>
+  );
+}
+
+function GameSymbol({ symbol }) {
+  return
+}
+
+function GameMoveInfo({ actions, currentMove, nextMove }) {
+  return (
+    <div className="flex gap-3 items-center">
+      <div className="mr-auto">
+        <div className="flex items-center gap-1 text-xl leading-tight text-slate-900">
+          Ход: {currentMove}
         </div>
-        <UiButton size="md" variant="primary">
-          Ничья
-        </UiButton>
-        <UiButton size="md" variant="outline">
-          Сдаться
-        </UiButton>
+        <div className="flex items-center gap-1 text-xs leading-tight text-slate-400">
+          Следующий: {nextMove}
+        </div>
       </div>
+      {actions}
+    </div>
+  );
+}
 
-      <div className="grid grid-cols-[repeat(19,_30px)] grid-rows-[repeat(19,_30px)] pl-px pt-px mt-3">
-        {cells.map((_, i) => (
-          <button key={i} className="border border-slate-200 -ml-px -mt-px flex items-center justify-center">
-
-          </button>
-        ))}
-      </div>
-
+function GameGrid({ children }) {
+  return (
+    <div className="grid grid-cols-[repeat(19,_30px)] grid-rows-[repeat(19,_30px)] pl-px pt-px mt-3">
+      {children}
     </div>
   );
 }
---
+++ components/game/game-symbol.jsx
@@ -0,0 +1,12 @@
+import { CrossIcon } from "./icons/cross-icon";
+import { TriangleIcon } from "./icons/triangle-icon";
+import { ZeroIcon } from "./icons/zero-icon";
+import { SquareIcon } from "./icons/square-icon";
+
+export function GameSymbol({ symbol }) {
+    const Icon {
+
+    }[symbol]
+
+    return;
+}
